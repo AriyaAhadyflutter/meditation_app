84543929f03c35484cb25a6a8c72850ca1039a56
ariaProgrammerFlutter <87ariyaahady@gmail.com>
2023-09-07 14:23:42 +0330

    create meditation app

---
+++ android/app/src/main/kotlin/com/example/meditationapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.meditationapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'constants/consts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Yekanplus',
+        scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: Theme.of(context).textTheme.apply(displayColor: kTextColor),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: size.height * 0.45,
+            decoration: const BoxDecoration(
+              color: Color(0xFFF5CEB8),
+              image: DecorationImage(
+                alignment: Alignment.center,
+                image: AssetImage(
+                  'assets/images/undraw_pilates_gpdb.png',
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
