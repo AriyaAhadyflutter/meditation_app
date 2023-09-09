baedc4c16ece21505896ed492e7b50062ebd15bc
ariaProgrammerFlutter <87ariyaahady@gmail.com>
2023-09-09 11:03:45 +0330

    keep pushig

--- lib/meditation_page.dart
+++ lib/meditation_page.dart
@@ -71,12 +71,12 @@ class MeditationPage extends StatelessWidget {
                     spacing: 20,
                     runSpacing: 20,
                     children: [
-                      BuildSession(cardnum: 'ویدیو شماره 01', isWached: true),
-                      BuildSession(cardnum: 'ویدیو شماره 02'),
-                      BuildSession(cardnum: 'ویدیو شماره 03'),
-                      BuildSession(cardnum: 'ویدیو شماره 04'),
-                      BuildSession(cardnum: 'ویدیو شماره 05'),
-                      BuildSession(cardnum: 'ویدیو شماره 06'),
+                      BuildSession(cardnum: 'ویدیو شماره 1', isWached: true),
+                      BuildSession(cardnum: 'ویدیو شماره 2'),
+                      BuildSession(cardnum: 'ویدیو شماره 3'),
+                      BuildSession(cardnum: 'ویدیو شماره 4'),
+                      BuildSession(cardnum: 'ویدیو شماره 5'),
+                      BuildSession(cardnum: 'ویدیو شماره 6'),
                     ],
                   ),
                 ],
@@ -92,53 +92,69 @@ class MeditationPage extends StatelessWidget {
 class BuildSession extends StatelessWidget {
   final String cardnum;
   final bool isWached;
+  final VoidCallback? press;
   const BuildSession({
     super.key,
     required this.cardnum,
     this.isWached = false,
+    this.press,
   });
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        return SingleChildScrollView(
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(13.0),
           child: Container(
             width: constraints.maxWidth / 2 - 10,
-            padding: const EdgeInsets.all(16),
-            decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(13)),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 17),
-                    blurRadius: 23,
-                    spreadRadius: -13,
-                    color: kShadowColor,
-                  ),
-                ]),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  cardnum,
-                  style: const TextStyle(
-                    fontFamily: 'iranSans',
-                    fontSize: 14,
-                  ),
-                ),
-                const SizedBox(width: 10),
-                Container(
-                  height: 42,
-                  width: 42,
-                  decoration: const BoxDecoration(
-                      color: kBlueColor, shape: BoxShape.circle),
-                  child: const Icon(
-                    Icons.play_arrow,
-                    color: Colors.white,
-                  ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(13.0),
+              boxShadow: const [
+                BoxShadow(
+                  offset: Offset(0.0, 17.0),
+                  blurRadius: 23.0,
+                  spreadRadius: -13.0,
+                  color: kShadowColor,
                 ),
               ],
+            ),
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: press,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        cardnum,
+                        style: const TextStyle(
+                          fontFamily: 'yagut',
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      const SizedBox(width: 10.0),
+                      Container(
+                        height: 42.0,
+                        width: 42.0,
+                        decoration: BoxDecoration(
+                          color: isWached ? kBlueColor : Colors.white,
+                          shape: BoxShape.circle,
+                          border: Border.all(color: kBlueColor),
+                        ),
+                        child: Icon(
+                          Icons.play_arrow,
+                          color: isWached ? Colors.white : kBlueColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
           ),
         );
